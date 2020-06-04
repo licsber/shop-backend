966c8f8a2c1dc63fcfa2e3ab84bc27401ae7c4b6
licsber <silverwings@foxmail.com>
2020-06-04 08:39:45 +0800

    init commit

---
+++ src/test/java/site/licsber/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package site.licsber.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
